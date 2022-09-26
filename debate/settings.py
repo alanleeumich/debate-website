a85a3cb74be985aab87438356f20acee81438577
alanleeumich <alanleee@umich.edu>
2022-09-26 14:54:22 -0400

    cleaned up AWS

--- debate/settings.py
+++ debate/settings.py
@@ -135,9 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/build/static')
+    os.path.join(BASE_DIR,'static')
 ]
 
 # Default primary key field type
