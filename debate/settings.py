4a55e990862ba6095439b70b01bb54c9744447f9
alanleeumich <alanleee@umich.edu>
2022-10-11 14:35:34 -0400

    changed database settings

--- debate/settings.py
+++ debate/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 
 from pickle import TRUE
 
@@ -91,14 +91,15 @@ ASGI_APPLICATION = 'debate.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-'''
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-}'''
-SQL_ALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+}
+DATABASES['default'] =  dj_database_url.config()
+#SQL_ALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
