0af6548528061e96aef8962c76043ed525b88e3d
alanleeumich <alanleee@umich.edu>
2022-10-11 14:24:43 -0400

    changed database settings

--- debate/settings.py
+++ debate/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+SQL_ALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
